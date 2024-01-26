4b13fc3f652574a01b76ffe398a02342e288dcc3
Андрей Рашев <ar42698937@edu.mon.bg>
2024-01-27 01:57:26 +0200

    Created Items for the machine

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Items List:");
+            Console.WriteLine("[A] " + "Water " + "- 1.00lv.");
+            Console.WriteLine("[B] " + "Brownie " + "- 2.50lv.");
+            Console.WriteLine("[C] " + "Napkins " + "- 0.50lv.");
+            Console.WriteLine("Please choose an item!");
+
+
+            string input = Console.ReadLine();
+
+            if (input == "a" || input == "A")
+            {
+
+            }
+
         }
     }
 }
